dab4663fe8ec87a4ce5672cf5943e1e804866319
Jordan Hart <jordandhart@gmail.com>
2020-06-30 00:54:25 -0700

    overlay attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,105 @@
 import 'package:flutter/material.dart';
 import 'face_detection_page.dart';
 
-void main() {
-  runApp(MyHomePage());
-}
+void main() => runApp(MyApp());
 
-class MyHomePage extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final items = const <BottomNavigationBarItem>[
-    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home Page')),
-    BottomNavigationBarItem(
-        icon: Icon(Icons.music_video), title: Text('Listen')),
-    BottomNavigationBarItem(icon: Icon(Icons.message), title: Text('message'))
-  ];
-  // This widget is the root of your application.
-  int currentIndex = 0;
-  List bodyList;
-  _MyHomePageState() {
-    bodyList = [FaceDetectionPage(), Text("2"), Text("3")];
-  }
-  void onTap(int index) {
-    setState(() {
-      currentIndex = index;
-    });
-  }
+class _MyAppState extends State<MyApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Testing background distance',
-        home: Scaffold(
-            bottomNavigationBar: BottomNavigationBar(
-                items: items, currentIndex: currentIndex, onTap: onTap),
-            // body: bodyList[currentIndex],
-            body: Stack(
-              children: [
-                Offstage(
-                  offstage: currentIndex != 0,
-                  child: bodyList[0],
-                ),
-                Offstage(
-                  offstage: currentIndex != 1,
-                  child: bodyList[1],
-                ),
-                Offstage(
-                  offstage: currentIndex != 2,
-                  child: bodyList[2],
-                ),
-              ],
-            )));
+      debugShowCheckedModeBanner: false,
+      home: WillPopScope(
+        onWillPop: () async => !await _navigatorKey.currentState.maybePop(),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            WidgetsBinding.instance
+                .addPostFrameCallback((_) => _insertOverlay(context));
+            return Navigator(
+              key: _navigatorKey,
+              onGenerateRoute: (RouteSettings settings) {
+                switch (settings.name) {
+                  case '/page2':
+                    return MaterialPageRoute(builder: (_) => Page2());
+                  default:
+                    return MaterialPageRoute(
+                        builder: (_) => Page1(_navigatorKey));
+                }
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _insertOverlay(BuildContext context) {
+    return Overlay.of(context).insert(
+      OverlayEntry(builder: (context) {
+        final size = MediaQuery.of(context).size;
+        print(size.width);
+        return Positioned(
+          width: 56,
+          height: 56,
+          top: size.height - 72,
+          left: size.width - 72,
+          child: Material(
+            color: Colors.transparent,
+            child: GestureDetector(
+              onTap: () => print('ON TAP OVERLAY!'),
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.redAccent),
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  Page1(this.navigatorKey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[200],
+      appBar: AppBar(title: Text('Page1')),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          children: [
+          FaceDetectionPage(),
+          RaisedButton(
+            child: Text('go to Page2'),
+            onPressed: () => navigatorKey.currentState.pushNamed('/page2'),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(title: Text('back to Page1')),
+      body: Container(
+        alignment: Alignment.center,
+        child: Text('Page 2'),
+      ),
+    );
   }
 }
